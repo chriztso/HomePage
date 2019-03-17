1cbdc25a84c8d1c4d6b20a6847bddf01b94ecf45
Chris Tso <christophertso2017@u.northwestern.edu>
2019-03-17 15:15:09 -0700

    Implemented media section of homepage

--- client/src/components/media.jsx
+++ client/src/components/media.jsx
@@ -1,20 +1,77 @@
 import React from 'react';
 import media from './media.css';
+import axios from 'axios';
 
 class MediaList extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-
+            allMedia : [],
+            toAddMedia : ''
         }
+
+    this.getMedia = this.getMedia.bind(this);
+    this.addMedia = this.addMedia.bind(this);
+    this.submitMedia = this.submitMedia.bind(this);
+    this.deleteMedia = this.deleteMedia.bind(this);    
     }
+
+  componentDidMount(){
+      this.getMedia();
+  }
+
+  getMedia(){
+      axios.get('/media')
+      .then((data) => {
+          console.log('HERE', data.data);
+          this.setState({allMedia : data.data});
+      })
+      .catch((err) => {
+          console.log(err);
+      })
+  }
+
+    addMedia(event){
+        this.setState({toAddMedia : event.target.value});
+    }
+
+    submitMedia(){
+        axios.post('/media', 
+        {
+            media : this.state.toAddMedia
+        })
+        .then(() => {this.getMedia()})
+        .catch((err) => {
+            console.log(err);
+        })
+    }
+
+    deleteMedia(){
+        axios.delete('/media')
+        .then(() => {this.getMedia()})
+        .catch((err) => {
+            console.log(err);
+        }) 
+    }  
     render(){
-        return (
+      var list = this.state.allMedia.map(media => 
+        <div className={media.mediaItem} > {media.media} 
+        <input type="submit" value="Edit" ></input>
+        <input type="submit" value="Delete"></input>
+        </div>  
+    );
+     return (
           <div className={media.mediaStyle}>
           <div className={media.mediaHeader}>
             <h3 className={media.mediaHeaderText}> Movies/TV Shows </h3>
+            <input type="submit" value="Clear" onClick = {this.deleteMedia}></input>
+            <img src="https://s3.us-east-2.amazonaws.com/chrismvp/video.png" className={media.image}/>
           </div>
-          <input type="text" className={media.mediaInput}></input>
+          <input type="text" className={media.mediaInput} onChange={this.addMedia}></input>
+          <input type="submit" value="add" onClick={this.submitMedia}></input>
+          <div>
+             {list}
+          </div>
           </div>   
         )
     }
