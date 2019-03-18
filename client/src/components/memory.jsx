58433c65887cd56f984b384161332f857ac6f3ce
Chris Tso <christophertso2017@u.northwestern.edu>
2019-03-18 14:32:41 -0700

    Edited photos section to render different photos on refresh

--- client/src/components/memory.jsx
+++ client/src/components/memory.jsx
@@ -1,17 +1,40 @@
 import React from 'react';
 import Memories from './memory.css';
+import Axios from 'axios';
+
 
 class Memory extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-
+            photo1 : '',
+            photo2 : ''
         }
+    this.getPhotos = this.getPhotos.bind(this);
     }
+
+    componentDidMount(){
+        this.getPhotos();
+    }
+
+    getPhotos(){
+        Axios.get('/photos')
+        .then((data) => {
+        var randomPhoto1 = Math.floor(Math.random() * data.data.length);
+        console.log(randomPhoto1);   
+        var randomPhoto2 = Math.floor(Math.random() * data.data.length);
+        console.log(randomPhoto2);
+        this.setState({photo1 : data.data[randomPhoto1]});
+        this.setState({photo2 : data.data[randomPhoto2]});
+        console.log(data.data)})
+        .catch((err) => {console.log(err)})
+    }
+
     render(){
         return(
             <div className={Memories.memoryStyle}> 
-               Photo
+               <img className = {Memories.memoryImage} src= {this.state.photo1.photos} />
+               <img className = {Memories.memoryImage} src={this.state.photo2.photos} />
             </div>
         )
     }
