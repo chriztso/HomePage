1706071d18a676e6c6f6f72056a536a69b0ce04f
Chris Tso <christophertso2017@u.northwestern.edu>
2019-03-18 22:54:09 -0700

    Implemented editing of music list items

--- client/src/components/musicList.jsx
+++ client/src/components/musicList.jsx
@@ -7,13 +7,18 @@ class MusicList extends React.Component{
         super(props);
         this.state = {
             allMusic: [],
-            toAddMusic: ''
+            toAddMusic: '',
+            toBeUpdated: '',
+            idNumber : ''
         }
     this.getMusic = this.getMusic.bind(this); 
     this.addMusic = this.addMusic.bind(this);  
     this.submitMusic = this.submitMusic.bind(this); 
     this.deleteMusic = this.deleteMusic.bind(this);  
-    this.deleteMusicOne = this.deleteMusicOne.bind(this);    
+    this.deleteMusicOne = this.deleteMusicOne.bind(this); 
+    this.updateMusic = this.updateMusic.bind(this);   
+    this.getId = this.getId.bind(this); 
+    this.submitUpdate = this.submitUpdate.bind(this); 
     }
     
 componentDidMount(){
@@ -59,9 +64,23 @@ deleteMusicOne(id){
   .then(() => {this.getMusic()})
   .catch((err) => {console.log(err);})
 }
+
+updateMusic(event){
+  this.setState({toBeUpdated : event.target.value});
+}
+
+getId(id){
+   this.setState({idNumber: id});
+}
+
+submitUpdate(){
+  axios.put('/updateMusic', {number : this.state.idNumber, text : this.state.toBeUpdated})
+  .then(() => {this.getMusic()})
+  .catch((err) => {console.log(err);})
+}
    
     render(){
-      
+
         return (
           <div className={music.musicStyle}>
           <div className={music.musicHeader}>
@@ -78,7 +97,7 @@ deleteMusicOne(id){
           <input type="text" className={music.musicInput} onChange={this.addMusic}></input>
           <input type="submit" value="+" onClick = {this.submitMusic} className={music.addButton}></input>
           <div>
-            <MusicList1 music={this.state.allMusic} deleteMusicOne={this.deleteMusicOne}/> 
+            <MusicList1 music={this.state.allMusic} deleteMusicOne={this.deleteMusicOne} updateMusic={this.updateMusic} getId={this.getId} submitUpdate={this.submitUpdate}/> 
           </div>
           </div>   
         )
@@ -89,23 +108,59 @@ deleteMusicOne(id){
 const MusicList1 = (props) => {
   return(
     props.music.map(music => 
-      <MusicItem1 music ={music} deleteMusicOne = {props.deleteMusicOne}/>
+      <MusicItem1 music ={music} deleteMusicOne = {props.deleteMusicOne} updateMusic={props.updateMusic} getId={props.getId} submitUpdate={props.submitUpdate}/>
     )
   )
 }
 
-const MusicItem1 = (props) => {
-return(
-  <div className={music.musicItem} > 
-  <div className={music.text} >
-    {props.music.music}
-  </div>
-    <span className={music.Buttons}>
-      <input type="submit" value="-" ></input>
-      <input type="submit" value="x" onClick={() => {props.deleteMusicOne(props.music.id)}}></input>
-    </span>
-  </div>  
-)
+
+class MusicItem1 extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      clicked : false
+    }
+this.handleClick = this.handleClick.bind(this);
+this.update = this.update.bind(this);
 }
 
+handleClick(){
+  this.setState({clicked : !this.state.clicked});  
+}
+
+update(){
+ this.props.submitUpdate();
+ this.setState({clicked : !this.state.clicked});  
+}
+
+render(){
+  var item; 
+  if(this.state.clicked === false){
+  item = 
+  <div className={music.innerDiv} onClick={() => {this.props.getId(this.props.music.id)}} >
+  <div className={music.text} >
+  {this.props.music.music}
+  </div>
+  <div className={music.Buttons}>
+    <input type="submit" value="-" onClick = {this.handleClick}></input>
+    <input type="submit" value="x" onClick={() => {this.props.deleteMusicOne(this.props.music.id)}}></input>
+  </div>
+  </div>
+  }
+  
+  if(this.state.clicked === true){
+  item = 
+  <div>
+   <input type = "text" onChange={this.props.updateMusic}></input> 
+   <input type = "submit" value="Edit" onClick={this.update}></input>  
+   </div> 
+  } 
+
+  return(
+    <div className = {music.musicItem}> 
+      {item}
+    </div>  
+  )
+ }
+}
 export default MusicList;
